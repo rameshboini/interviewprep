b0de419e576a68697493047602acfb14c474a9e6
Ramesh Boini <hairam1066@gmail.com>
2021-09-28 16:34:08 +0530

    sorted stack v2

--- stack/sortstack.cpp
+++ stack/sortstack.cpp
@@ -1,64 +1,79 @@
 /*Given a stack, the task is to sort it such that the top of the stack has the greatest element.*/
 
 //GFG-100 7
-#include "linkedlist.h"
 
-void push(Node **stack, int data)
-{
-    Node *tmp = new Node(data);
-    tmp->next = *stack;
-    *stack = tmp;
-}
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
 
-int pop(Node **stack)
-{
-    int ret = (*stack)->data;
-    Node *de = (*stack);
-    *stack = (*stack)->next;
-    delete de;
-    return ret;
-}
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
 
-void printStack(Node *head)
+void printStack(stack<int> s)
 {
-    while (head)
+    while (!s.empty())
     {
-        cout << head->data << " ";
-        head = head->next;
+        printf("%d ", s.top());
+       	s.pop();
     }
-    cout << endl;
-}
-
-Node *sortStack(Node *stack)
-{
-    Node *tmp = nullptr;
-
-    while (stack)
-    {
-        int x = pop(&stack);
-
-        while (tmp && tmp->data > x)
-        {
-            push(&stack,pop(&tmp));
-        }
-        push(&tmp,x);
-    }
-    return tmp;
+    printf("\n");
 }
 
 int main()
 {
-
-    Node *stack = new Node(10);
-    push(&stack, 5);
-    push(&stack, 50);
-    push(&stack, 2);
-    push(&stack, 1);
-    push(&stack, 8);
-    push(&stack, 6);
-    push(&stack, 55);
-
-    printStack(stack);
-
-    printStack(sortStack(stack));
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
 }
+}// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void sortInternal(stack<int>&s,int ele){
+   
+   if(s.empty() || ele > s.top()){
+       s.push(ele);
+   }else{
+       int temp = s.top();
+       s.pop();
+       sortInternal(s,ele);
+       s.push(temp);
+       return;
+   }
+   
+}
+void SortedStack :: sort()
+{
+    if(!s.empty())
+    {
+        int x = s.top();
+        s.pop();
+        sort();
+        sortInternal(s, x);
+    }
+}
+
